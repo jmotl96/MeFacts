2cef2152d0541f8a5e3c4826162903ef3e51ba02
jmotl96 <35705892+jmotl96@users.noreply.github.com>
2021-05-03 15:24:59 -0500

    Initial commit

---
+++ MeFacts/MeFacts/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MeFacts
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            MeFactsData fact = (MeFactsData)e.SelectedItem;
+            DisplayAlert("The Fact", fact.TheFact, "Ok");
+        }
+    }
+}
---
+++ MeFacts/MeFacts/MeFactsData.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace MeFacts
+{
+    public class MeFactsData
+    {
+        public MeFactsData()
+        {
+        }
+        public static IEnumerable<MeFactsData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        static MeFactsData()
+        {
+            List<MeFactsData> all = new List<MeFactsData>();
+            all.Add(new MeFactsData() { TheFact = "Cook is extreamly fun for me ", ShortFact = "I like to cook",});
+            all.Add(new MeFactsData() { TheFact = "Gaming is what got me into IT in the first place", ShortFact = "Gaming" });
+            all.Add(new MeFactsData() { TheFact = "I am on the sprecriom ", ShortFact = "autsiom" });
+            all.Add(new MeFactsData() { TheFact = "My deam job is to be an astront", ShortFact = "Dream job" });
+            all.Add(new MeFactsData() { TheFact = "I like Both", ShortFact = "Star Wars or star trek" });
+            All = all;
+
+        }
+    }
+}
